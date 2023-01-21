ce210c0c1faddafea4064ab71675b4c1bded79c0
Mohamad <mohamadkassem101@gmail.com>
2023-01-21 14:40:48 +0200

    'deployment'

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -8,19 +8,19 @@ const HomePage = () => {
         style={{ height: "calc(100vh - 64px)" }}
       >
         <div
-          className="relative container px-4   mx-auto"
+          className="relative container px-4 w-auto h-auto  mx-auto"
           style={{ top: "25%" }}
         >
-          <div className="flex flex-wrap items-center -mx-4 mb-10 2xl:mb-14">
+          <div className="flex  items-center ">
             <div className="w-full lg:w-1/2 px-4 mb-16 lg:mb-0">
-              <span className="text-lg font-bold text-blue-400">
+              <span className=" font-bold text-blue-400">
                 Create posts to educate
               </span>
-              <h2 className="max-w-2xl mt-12 mb-12 text-6xl 2xl:text-8xl text-white font-bold font-heading">
+              <h2 className=" my-2 text-2xl  text-white font-bold font-heading">
                 Pen down your ideas{" "}
                 <span className="text-yellow-500">By creating a post</span>
               </h2>
-              <p className="mb-12 lg:mb-16 2xl:mb-24 text-xl text-gray-100">
+              <p className=" lg:mb-16 2xl:mb-24 text-lg text-gray-100">
                 Your post must be free from racism and unhealthy words
               </p>
             </div>
